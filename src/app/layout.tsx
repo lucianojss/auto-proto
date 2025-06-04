4649e7b4cfd49719922d466d8f020cad2e874f1f
Luciano Santos <luciusjss@gmail.com>
2025-06-04 10:18:44 +0100

    feat: moved favicon.ico to /app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
