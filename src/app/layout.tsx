8f69ebbd777111b8e69d8d59708ed5e651e6595c
Luciano Santos <luciusjss@gmail.com>
2025-06-04 09:43:48 +0100

    feat: added link tag on head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
